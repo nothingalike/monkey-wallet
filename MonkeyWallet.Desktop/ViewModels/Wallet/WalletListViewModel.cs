6d4db86ff86cfd9a135f8c1914bd64b707946df5
Daniel-SchaeferJ <46212174+Daniel-SchaeferJ@users.noreply.github.com>
2023-03-21 13:46:33 -0700

    Fixed the nav bar

---
+++ MonkeyWallet.Desktop/ViewModels/Wallet/WalletListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonkeyWallet.Desktop.ViewModels.Wallet;
+
+public class WalletListViewModel
+{
+}
---
+++ MonkeyWallet.Desktop/Views/Wallet/WalletListView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MonkeyWallet.Desktop.Views.Wallet;
+
+public partial class WalletListView : UserControl
+{
+    public WalletListView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
