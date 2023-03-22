58f8403c1ce274abeef6e077c2c0c8db6a8c380c
Kyle Johns <kjohns1337@gmail.com>
2023-03-21 23:03:02 -0400

    added show mnemonic

--- MonkeyWallet.Desktop/ViewModels/Wallet/ShowMnemonicViewModel.cs
+++ MonkeyWallet.Desktop/ViewModels/Wallet/ShowMnemonicViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MonkeyWallet.Desktop.ViewModels.Wallet;
 
@@ -11,4 +12,9 @@ public class ShowMnemonicViewModel : ViewModelBase, IRoutableViewModel
 {
     public string UrlPathSegment => nameof(AddWalletViewModel);
     public IScreen HostScreen { get; }
+
+    public List<string> Mnemonic { get; set; }
+
+    public ICommand Previous { get; set; }
+    public ICommand Next { get; set; }
 }
