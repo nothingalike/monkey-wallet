49cb37f93464d776fe3179eff72a47a47b9f1a54
Daniel-SchaeferJ <46212174+Daniel-SchaeferJ@users.noreply.github.com>
2023-04-24 21:59:23 -0700

    Getting start on receive fund

---
+++ MonkeyWallet.Desktop/ViewModels/Wallet/ReceiveFunds/BaseReceiveFundsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReactiveUI;
+
+namespace MonkeyWallet.Desktop.ViewModels.Wallet.ReceiveFunds;
+
+public class BaseReceiveFundsViewModel  : ReactiveObject, IActivatableViewModel, IScreen
+{
+    public ViewModelActivator Activator { get; } = new();
+    public RoutingState Router { get; } = new();
+
+    public BaseReceiveFundsViewModel()
+    {
+        Router.Navigate.Execute(new ReceiveFundListViewModel(this));
+    }
+}
---
+++ MonkeyWallet.Desktop/ViewModels/Wallet/ReceiveFunds/ReceiveFundListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReactiveUI;
+
+namespace MonkeyWallet.Desktop.ViewModels.Wallet.ReceiveFunds;
+
+public class ReceiveFundListViewModel : ViewModelBase, IRoutableViewModel
+{
+    public string? UrlPathSegment => nameof(ReceiveFundListViewModel);
+    public IScreen HostScreen { get; }
+
+    public ReceiveFundListViewModel(IScreen screen)
+    {
+        HostScreen = screen;
+    }
+}
